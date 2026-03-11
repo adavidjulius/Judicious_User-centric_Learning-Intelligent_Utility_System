9fe8f22f1b78b0397ea23bd858fd74a24373e756
David Julius <davidjulius246@gmail.com>
2026-03-11 19:35:36 +0530

    Delete tts_engine.py

--- tts_engine.py
+++
@@ -1,8 +0,0 @@
-from TTS.api import TTS
-
-tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC")
-
-def speak(text):
-    file_path = "response.wav"
-    tts.tts_to_file(text=text, file_path=file_path)
-    return file_path
